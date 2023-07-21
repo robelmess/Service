3ece47bd7ba2150ecd35360e8554a59ba1ceaa45
Be-Emnet Elias <belias@uw.edu>
2023-07-21 14:51:10 -0700

    fixed slowness

--- Screens/Details.js
+++ Screens/Details.js
@@ -1,183 +1,104 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Linking, Image } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Linking, Image, Dimensions } from 'react-native';
 import { Icon } from 'react-native-elements';
+import Swiper from 'react-native-swiper';
 
-export default function DetailsScreen({ navigation, route }) {
-    const { selectedDay, description, color, legendTag, icon, link} = route.params;
 
-    // Correct the off-by-one error in the date and format it
+export default function DetailsScreen({ route }) {
+    const { selectedDay, description, color, legendTag, icon,link,title } = route.params;
+
     const correctedDate = new Date(selectedDay);
     correctedDate.setDate(correctedDate.getDate() + 1);
     const formattedDate = correctedDate.toLocaleDateString('en-US', { weekday: 'long', month: 'short', day: 'numeric' });
 
     return (
         <View style={[styles.detailsScreenContainer, { backgroundColor: color }]}>
-            <View style={styles.legendTagContainer}>
-                <View style={styles.detailsHeader}>
-                    <Text
-                        style={styles.legendTagText}
-                        numberOfLines={1}
-                        adjustsFontSizeToFit
-                    >
-                        {legendTag}
-                    </Text>
+            <Swiper showsButtons={false} loop={false} activeDotColor="white">
+                <View style={styles.slide}>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            style={styles.image}
+                            source={{ uri: icon }}
+                        />
+                        <View style={styles.titleContainer}>
+                            <Text style={styles.titleText}>{title}</Text>
+                            <View style={styles.dateContainer}>
+                                <Icon name="calendar" type="font-awesome" size={20} color="white" />
+                                <Text style={styles.dateText}>{formattedDate}</Text>
+                            </View>
+                        </View>
+                        <TouchableOpacity style={styles.infoIcon} onPress={() => Linking.openURL(link)}>
+                            <Icon name="info" type="font-awesome" size={20} color="black" />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-
-            <View style={styles.dateContainer}>
-                <Icon name="calendar" type="font-awesome" size={20} color="white" />
-                <Text style={styles.dateText}>{formattedDate}</Text>
-                <Icon name="bell" type="font-awesome" size={20} color="white" />
-                <Text style={styles.dateText}>Set Reminder</Text>
-            </View>
-
-            <View style={styles.iconContainer}>
-                <Image
-                    style={styles.icon}
-                    source={{ uri: icon }}
-                />
-            </View>
-            <ScrollView style={styles.descriptionContainer}>
-                <Text style={styles.descriptionText}>{description}</Text>
-            </ScrollView>
-            <TouchableOpacity style={styles.returnButton} onPress={() => Linking.openURL(link)}>
-                <Text style={styles.returnButtonText}>Learn More</Text>
-            </TouchableOpacity>
+                <View style={styles.slide}>
+                    <View style={[styles.descriptionContainer, { backgroundColor: color }]}>  
+                        <Text style={styles.descriptionText}>{description}</Text> 
+                    </View>
+                </View>
+                
+            </Swiper>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    detailsHeader: {
-      width: '100%',
-      borderBottomWidth: 1,
-      paddingBottom: 5,
-      paddingLeft: 20,
-    },
-    iconContainer: {
-      justifyContent: 'center',
-      width: '100%',
-      aspectRatio: 2,
-      padding: 10,
-    },
-    icon: {
-      borderRadius: 50,
-      flex: 1,
-      resizeMode: 'cover'
-    },
-    legendWrapper: {
-      flexDirection: 'row',
-      position: 'absolute',
-      bottom: 20,
-    },
-    jumpToTodayButton: {
-      backgroundColor: '#FFF',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 25,
-      elevation: 5,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.3,
-      shadowRadius: 2,
-      position: 'absolute', // <-- Add this
-      bottom: 20, // <-- Add this, adjust as needed
-      right: 20, // <-- Add this, adjust as needed
-    },
-  
-    jumpToTodayButtonText: {
-      color: '#000',
-      fontSize: 16,
-    },
     detailsScreenContainer: {
-      flex: 1,
+        flex: 1,
+        padding: 10,
     },
-    legendTagContainer: {
-      alignItems: 'center',
-      marginBottom: 10,
+    imageContainer: {
+        borderRadius: 10,
+        overflow: 'hidden',
+        marginBottom: 10,
+        position: 'relative',
     },
-    legendTagText: {// details title
-      fontSize: 50,
-      fontWeight: 'bold',
-      color: 'white',
-      overflow: 'hidden',
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
     },
+    titleContainer: {
+        position: 'absolute',
+        left: 10,
+        bottom: 40,
+        padding: 5,
+        borderRadius: 5,
+    },
+    titleText: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    dateText: {
+        fontSize: 20,
+        color: 'white',
+        left: 10,
+    },
+    infoIcon: {
+        position: 'absolute',
+        right: 10,
+        top: 10,
+        width: 30,
+        backgroundColor: 'white',
+        borderRadius: 15,
+        padding: 5,
+    },
+    descriptionContainer: {
+        borderRadius: 15,
+        padding: 10,
+    },
+    descriptionText: {
+        fontSize: 20,
+        color: 'white',
+        fontFamily: 'Verdana',
+        lineHeight: 24,
+        marginBottom: 10,
+    },    
     dateContainer: {
       flexDirection: 'row',
       alignItems: 'center',
-      marginBottom: 10,
-      paddingLeft: 20,
-    },
-    dateText: {
-      paddingLeft: 10,
-      fontSize: 10,
-      color: 'white',
-      paddingRight: 10
-    },
-    descriptionContainer: {
-      flex: 1,
-      marginBottom: 10,
-      padding: 20
-    },
-    descriptionText: {
-      fontSize: 20,
-      color: 'white',
-      textAlign: 'justify'
-    },
-    returnButton: {
-      alignSelf: 'center',
-      marginBottom: 20,
-      backgroundColor: 'white',
-      padding: 10,
-      borderRadius: 5,
-    },
-    returnButtonText: {
-      fontSize: 16,
-      color: 'black',
-    },
-    legendContainer: {
-      flexDirection: 'row',
-      flex: 1,
-      backgroundColor: 'white',
-      borderRadius: 10,
-      padding: 10,
-      alignItems: 'flex-start',
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 1 },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.5,
-      bottom: 55,
-      flexWrap: 'wrap',
-      marginLeft: 10,
-      marginRight: 10,
-    },
-    legendTitle: {
-      color: 'black',
-      fontWeight: 'bold',
-    },
-    legendItemContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-    },
-    legendColorDot: {
-      width: 10,
-      height: 10,
-      borderRadius: 5,
-      marginRight: 5
-    },
-    legendColorLabel: {
-      fontSize: 12,
-      // whiteSpace: 'nowrap', // <-- Prevent text from breaking to the new line
-    },
-    jumpToTodayText: {
-      fontSize: 16,
-      color: 'black',
-    },
-    legendColorDotWrapper: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 5,
-    },
-  });
-  
+  },
+});
+
--- Screens/Home.js
+++ Screens/Home.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { useState, useRef, useEffect } from 'react'
 import { CalendarList } from 'react-native-calendars';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import database from '@react-native-firebase/database';
 
 function User({ userId }) {
@@ -13,304 +11,257 @@ function User({ userId }) {
 export default function HomeScreen({ navigation }) {
   const [Data, setData] = useState()
 
-  database()
-    .ref(`/Events`)
-    .on('value', snapshot => {
-      setData(expandData(snapshot.val()))
-    });
-
-  const [selectedDay, setSelectedDay] = useState()
-  const [currentMonth, setCurrentMonth] = useState();
-  const calendarRef = useRef();
-
-  const handleVisibleMonthsChange = (months) => {
-    if (months && months.length > 0) {
-      const dateString = months[0].dateString;
-      if (dateString && typeof dateString === 'string') {
-        const dateParts = dateString.split("-");
-
-        // Ensure dateParts has at least 2 elements (year and month)
-        if (dateParts.length >= 2) {
-          setCurrentMonth(dateParts[1]);
+  useEffect(() => {
+    const ref = database().ref('/Events');
+    const handleValueChange = snapshot => {
+      setData(expandData(snapshot.val()));
+    };
+  
+    ref.on('value', handleValueChange);
+  
+    // Clean up function
+    return () => {
+      ref.off('value', handleValueChange);
+    };
+  }, []);
+  
+    const [currentMonth, setCurrentMonth] = useState();
+    const calendarRef = useRef();
+  
+  
+    const handleVisibleMonthsChange = (months) => {
+      if (months && months.length > 0) {
+        const dateString = months[0].dateString;
+        if (dateString && typeof dateString === 'string') {
+          const dateParts = dateString.split("-");
+  
+          // Ensure dateParts has at least 2 elements (year and month)
+          if (dateParts.length >= 2) {
+            setCurrentMonth(dateParts[0] + "-" + dateParts[1]);
+          }
+  
         }
       }
-    }
-  };
-
-  function expandData(data) {
-    const expandedData = {};
-
-    for (const item of data) {
-      const { Date: date, Color: color, ...rest } = item;
-
-      if (date.includes('|')) {
-        const [startDate, endDate] = date.split('|');
-        let d = new Date(startDate);
-        let end = new Date(endDate);
-
-        for (; d <= end; d = new Date(d.getTime() + 24 * 60 * 60 * 1000)) {
-          const newDate = d.toISOString().split('T')[0];
-
-          expandedData[newDate] = {
+    };
+  
+  
+    function expandData(data) {
+      const expandedData = {};
+    
+      for (const item of data) {
+        const { Date: date, Color: color, ...rest } = item;
+    
+        if (date.includes('|')) {
+          const [startDate, endDate] = date.split('|');
+          let d = new Date(startDate);
+          let end = new Date(endDate);
+    
+          for (; d <= end; d = new Date(d.getTime() + 24 * 60 * 60 * 1000)) {
+            const newDate = d.toISOString().split('T')[0];
+    
+            expandedData[newDate] = {
+              ...rest,
+              color,
+              Date: newDate,
+              textColor: 'white',
+              startingDay: d.getTime() === new Date(startDate).getTime(),
+              endingDay: d.getTime() === end.getTime(),
+            };
+          }
+        } else {
+          expandedData[date] = {
             ...rest,
             color,
-            Date: newDate,
             textColor: 'white',
-            startingDay: d.getTime() === new Date(startDate).getTime(),
-            endingDay: d.getTime() === end.getTime(),
+            startingDay: true,
+            endingDay: true,
           };
         }
-      } else {
-        expandedData[date] = {
-          ...rest,
-          color,
-          textColor: 'white',
-          startingDay: true,
-          endingDay: true,
-        };
       }
+    
+      return expandedData;
     }
-
-    return expandedData;
-  }
-
-
-
-
-
-  const getLegendForMonth = (month) => {
-    if (!month) {
-      return []
-    }
-    // Ensure month is always two digits (e.g. '07' instead of '7')
-    month = month.toString().padStart(2, '0');
-
-    // Use an object to store unique colors with their corresponding legend tags
-    const uniqueColors = {};
-
-    // Map through the marked dates and add the unique colors to the object
-    for (const date in Data) {
-      if (date.startsWith(`2023-${month}`)) {
-        const { color, legendTag } = Data[date];
-        if (!uniqueColors[color]) {
-          uniqueColors[color] = legendTag;
+    
+  
+    const getLegendForMonth = (month) => {
+      if (!month) {
+        return []
+      }
+      // Use an object to store unique colors with their corresponding legend tags
+      const uniqueColors = {};
+      
+      // Map through the marked dates and add the unique colors to the object
+      for (const date in Data) {
+        if (date.startsWith(month)) { // <-- Corrected this line
+          const { color, LegendTag } = Data[date];
+          if (!uniqueColors[color]) {
+            uniqueColors[color] = LegendTag;
+          }
         }
       }
-    }
+      // Convert the object back to an array of {color, legendTag} objects and return it
+      return Object.keys(uniqueColors).map(color => ({ color, legendTag: uniqueColors[color] }));
+    };
+    
+    
+  
+  
+    const handleJumpToToday = React.useCallback(() => {
+      const firstDayOfCurrentMonth = new Date();
+      firstDayOfCurrentMonth.setDate(1);
+      const year = firstDayOfCurrentMonth.getFullYear();
+      const month = (firstDayOfCurrentMonth.getMonth() + 1).toString().padStart(2, '0');
+      const targetDate = `${year}-${month}-01`;
+      calendarRef.current.scrollToDay(targetDate, 0, true);
+    }, []);
+    
+  
+    const Legend = React.memo(({ month }) => {
+      const legendItems = React.useMemo(() => getLegendForMonth(currentMonth), [currentMonth]);
 
-    // Convert the object back to an array of {color, legendTag} objects and return it
-    return Object.keys(uniqueColors).map(color => ({ color, legendTag: uniqueColors[color] }));
-  };
-
-
-  const handleJumpToToday = () => {
-    const firstDayOfCurrentMonth = new Date();
-    firstDayOfCurrentMonth.setDate(1);
-    const year = firstDayOfCurrentMonth.getFullYear();
-    const month = (firstDayOfCurrentMonth.getMonth() + 1).toString().padStart(2, '0');
-    const targetDate = `${year}-${month}-01`;
-    calendarRef.current.scrollToDay(targetDate, 0, true);
-  };
-
-  const Legend = React.memo(({ month }) => {
-    const legendItems = React.useMemo(() => getLegendForMonth(month), [month]);
-
+      return (
+        <View style={styles.legendContainer}>
+          {legendItems.map((item, index) => (
+            <View key={index} style={styles.legendColorDotWrapper}>
+              <View
+                style={[
+                  styles.legendColorDot,
+                  { backgroundColor: item.color },
+                ]}
+              />
+              <Text style={styles.legendColorLabel}>{item.legendTag}</Text>
+            </View>
+          ))}
+        </View>
+      );
+    });
+  
+  
+  
     return (
-      <View style={styles.legendContainer}>
-        {legendItems.map((item, index) => (
-          <View key={index} style={styles.legendColorDotWrapper}>
-            <View
-              style={[
-                styles.legendColorDot,
-                { backgroundColor: item.color },
-              ]}
-            />
-            <Text style={styles.legendColorLabel}>{item.legendTag}</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <CalendarList
+  
+            onDayPress={day => {
+              const pressedDate = day.dateString;
+              let selectedDate = pressedDate;
+              let dateDetails = Data[selectedDate] || {};
+  
+              if (dateDetails.color) {
+                // If the selected date does not have details, check previous dates
+                while (!dateDetails.Description && selectedDate > '2023-01-01') {
+                  const prevDate = new Date(selectedDate);
+                  prevDate.setDate(prevDate.getDate() - 1);
+                  selectedDate = prevDate.toISOString().split('T')[0];
+                  dateDetails = Data[selectedDate] || {};
+                }
+  
+                const { Description, color, LegendTag, Icon, Link, Title} = dateDetails;
+                navigation.navigate('Details', { selectedDay: pressedDate, description: Description, color: color, legendTag: LegendTag, icon: Icon, link: Link, title: Title});
+              }
+            }}
+  
+            current='2023-01-01'
+  
+            onVisibleMonthsChange={handleVisibleMonthsChange}
+            ref={calendarRef}
+            markingType={'period'}
+  
+            minDate='2023-01-01'
+            maxDate='2025-12-31'
+            pastScrollRange={9}
+            futureScrollRange={12}
+  
+            markedDates={Data}
+          />
+          <View style={styles.legendWrapper}>
+            <Legend month={currentMonth} />
           </View>
-        ))}
+          <TouchableOpacity onPress={handleJumpToToday} style={styles.jumpToTodayButton}>
+            <Text style={styles.jumpToTodayButtonText}>Today</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
+  }
+  
+  const styles = StyleSheet.create({
+    legendWrapper: {
+      flexDirection: 'row',
+      position: 'absolute',
+      bottom: 20,
+    },
+    jumpToTodayButton: {
+      backgroundColor: '#FFF',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 25,
+      elevation: 5,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.3,
+      shadowRadius: 2,
+      position: 'absolute', // <-- Add this
+      bottom: 20, // <-- Add this, adjust as needed
+      right: 20, // <-- Add this, adjust as needed
+    },
+  
+    jumpToTodayButtonText: {
+      color: '#000',
+      fontSize: 16,
+    },
+    legendTagContainer: {
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+    legendTagText: {// details title
+      fontSize: 50,
+      fontWeight: 'bold',
+      color: 'white',
+      overflow: 'hidden',
+    },
+    legendContainer: {
+      flexDirection: 'row',
+      flex: 1,
+      backgroundColor: 'white',
+      borderRadius: 10,
+      alignItems: 'flex-start',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.5,
+      bottom: 55,
+      flexWrap: 'wrap',
+      marginLeft: 10,
+      marginRight: 10,
+    },
+    legendTitle: {
+      color: 'black',
+      fontWeight: 'bold',
+    },
+    legendItemContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    legendColorDot: {
+      width: 10,
+      height: 10,
+      borderRadius: 5,
+      marginRight: 5
+    },
+    legendColorLabel: {
+      fontSize: 12,
+      // whiteSpace: 'nowrap', // <-- Prevent text from breaking to the new line
+    },
+    jumpToTodayText: {
+      fontSize: 16,
+      color: 'black',
+    },
+    legendColorDotWrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+    },
   });
-
-
-
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <CalendarList
-
-          onDayPress={day => {
-            const pressedDate = day.dateString;
-            let selectedDate = pressedDate;
-            let dateDetails = Data[selectedDate] || {};
-
-            if (dateDetails.color) {
-              // If the selected date does not have details, check previous dates
-              while (!dateDetails.Description && selectedDate > '2023-01-01') {
-                const prevDate = new Date(selectedDate);
-                prevDate.setDate(prevDate.getDate() - 1);
-                selectedDate = prevDate.toISOString().split('T')[0];
-                dateDetails = Data[selectedDate] || {};
-              }
-
-              const { Description, color, LegendTag, Icon, Link } = dateDetails;
-              navigation.navigate('Details', { selectedDay: pressedDate, description: Description, color: color, legendTag: LegendTag, icon: Icon, link: Link });
-            }
-          }}
-
-          current='2023-01-01'
-
-          onVisibleMonthsChange={handleVisibleMonthsChange}
-          ref={calendarRef}
-          markingType={'period'}
-
-          minDate='2023-01-01'
-          maxDate='2025-12-31'
-          pastScrollRange={9}
-          futureScrollRange={12}
-
-          markedDates={Data}
-        />
-        <View style={styles.legendWrapper}>
-          <Legend month={currentMonth} />
-        </View>
-        <TouchableOpacity onPress={handleJumpToToday} style={styles.jumpToTodayButton}>
-          <Text style={styles.jumpToTodayButtonText}>Today</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  detailsHeader: {
-    width: '100%',
-    borderBottomWidth: 1,
-    paddingBottom: 5,
-    paddingLeft: 20,
-  },
-  iconContainer: {
-    justifyContent: 'center',
-    width: '100%',
-    aspectRatio: 2,
-    padding: 10,
-  },
-  icon: {
-    borderRadius: 50,
-    flex: 1,
-    resizeMode: 'cover'
-  },
-  legendWrapper: {
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 20,
-  },
-  jumpToTodayButton: {
-    backgroundColor: '#FFF',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 25,
-    elevation: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    position: 'absolute', // <-- Add this
-    bottom: 20, // <-- Add this, adjust as needed
-    right: 20, // <-- Add this, adjust as needed
-  },
-
-  jumpToTodayButtonText: {
-    color: '#000',
-    fontSize: 16,
-  },
-  detailsScreenContainer: {
-    flex: 1,
-  },
-  legendTagContainer: {
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  legendTagText: {// details title
-    fontSize: 50,
-    fontWeight: 'bold',
-    color: 'white',
-    overflow: 'hidden',
-  },
-  dateContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    paddingLeft: 20,
-  },
-  dateText: {
-    paddingLeft: 10,
-    fontSize: 10,
-    color: 'white',
-    paddingRight: 10
-  },
-  descriptionContainer: {
-    flex: 1,
-    marginBottom: 10,
-    padding: 20
-  },
-  descriptionText: {
-    fontSize: 20,
-    color: 'white',
-    textAlign: 'justify'
-  },
-  returnButton: {
-    alignSelf: 'center',
-    marginBottom: 20,
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 5,
-  },
-  returnButtonText: {
-    fontSize: 16,
-    color: 'black',
-  },
-  legendContainer: {
-    flexDirection: 'row',
-    flex: 1,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    alignItems: 'flex-start',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.5,
-    bottom: 55,
-    flexWrap: 'wrap',
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  legendTitle: {
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  legendItemContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  legendColorDot: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    marginRight: 5
-  },
-  legendColorLabel: {
-    fontSize: 12,
-    // whiteSpace: 'nowrap', // <-- Prevent text from breaking to the new line
-  },
-  jumpToTodayText: {
-    fontSize: 16,
-    color: 'black',
-  },
-  legendColorDotWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-  },
-});
