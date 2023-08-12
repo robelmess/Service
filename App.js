2934faecce728843857dd1870dd82cf323747255
Robel Messele <robel@Robels-MacBook-Air.local>
2023-08-11 20:45:53 -0700

    firwt

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './Screens/Home';
-import DetailsScreen from './Screens/Details'
 
 import { useState, useRef, useEffect } from 'react'
-import { useColorScheme, StyleSheet } from 'react-native';
+import { useColorScheme, StyleSheet, View } from 'react-native';
 
 import { requestUserPermission, NotificationListener, GetFCMToken, checkToken, getFCMToken } from "./utils/pushnotification_helper"
 
@@ -20,12 +18,7 @@ function App() {
   }, [])
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="EOTC Calendar">
-        <Stack.Screen options={theme == 'light' ? styles.light : styles.dark} name="Orthodox Tewahedo Calendar" component={HomeScreen} />
-        <Stack.Screen options={{ headerShown: false, presentation: 'modal' }} name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <View style={{flex: 1, backgroundColor:'blue'}}/>
   );
 }
 
